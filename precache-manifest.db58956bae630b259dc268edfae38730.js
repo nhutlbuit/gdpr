0d44f393906af78e1e082acae6f79a651f941c55
LBN <nhutlbuit@LBNs-MacBook-Pro.local>
2020-08-22 17:24:43 +0700

    Updates

---
+++ precache-manifest.db58956bae630b259dc268edfae38730.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a924d96880faea7e4c0920e16fe4aa",
+    "url": "/bo-gdpr/index.html"
+  },
+  {
+    "revision": "ca7d5c4ca4ebf73c66c2",
+    "url": "/bo-gdpr/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "613f235261902aa96878",
+    "url": "/bo-gdpr/static/css/3.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e352924e2ff52cbe997e",
+    "url": "/bo-gdpr/static/css/main.1a74d49e.chunk.css"
+  },
+  {
+    "revision": "ca7d5c4ca4ebf73c66c2",
+    "url": "/bo-gdpr/static/js/2.82b66786.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/bo-gdpr/static/js/2.82b66786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613f235261902aa96878",
+    "url": "/bo-gdpr/static/js/3.a6881f92.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/bo-gdpr/static/js/3.a6881f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7223cffb203b012476d",
+    "url": "/bo-gdpr/static/js/4.ac801f98.chunk.js"
+  },
+  {
+    "revision": "e352924e2ff52cbe997e",
+    "url": "/bo-gdpr/static/js/main.951f3f3e.chunk.js"
+  },
+  {
+    "revision": "0e2d591614703b55801c",
+    "url": "/bo-gdpr/static/js/runtime-main.55edc9ea.js"
+  }
+]);
